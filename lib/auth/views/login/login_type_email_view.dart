68c98af7a8c101a941128a067ce0ab972aecb3ed
stevehighly0 <stevehighly0>
2023-02-13 02:57:03 +0300

    Added login views

--- lib/auth/views/login/login_type_email_view.dart
+++ lib/auth/views/login/login_type_email_view.dart
@@ -4,8 +4,8 @@ import 'package:pdf_editor/auth/bloc/auth_event.dart';
 
 import 'package:pdf_editor/auth/bloc/enums/auth_page.dart';
 import 'package:pdf_editor/auth/bloc/enums/auth_type.dart';
-import 'package:pdf_editor/auth/generics/views/generic_type_email_view.dart';
 
+import '../../../helpers/loading/loading_screen.dart';
 import '../../bloc/auth_bloc.dart';
 import '../../generics/views/generic_auth_view.dart';
 
@@ -14,19 +14,25 @@ class LoginTypeEmailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 1)).then((value) {
+      LoadingScreen().show(context: context, text: "hhh");
+    });
     return GenericAuthView(
-      authPage: AuthPage.onTypingEmailPage,
-      authType: AuthType.login,
       onProceed: () {
         context.read<AuthBloc>().add(
               AuthEventTypePassword(
-                isTextObscure: false,
+                isTextObscure: true,
                 textFieldBorderColor: const Color.fromRGBO(186, 186, 186, 100),
                 authPage: AuthPage.onTypingPasswordPage,
                 isFieldValid: false,
                 authType: AuthType.login,
               ),
             );
+      },
+      onBack: () {
+        context
+            .read<AuthBloc>()
+            .add(const AuthEventSeekMain(shouldSkipButtonGlow: false));
       },
     );
   }
