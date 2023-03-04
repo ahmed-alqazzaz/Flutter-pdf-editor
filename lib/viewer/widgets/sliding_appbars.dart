0b9787f007e934a52b4e3e49e33e09d08f681998
stevehighly0 <stevehighly0>
2023-03-04 13:02:04 +0300

    added a temporary popup menu button to appbar

--- lib/viewer/widgets/sliding_appbars.dart
+++ lib/viewer/widgets/sliding_appbars.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-
+import 'package:rxdart/rxdart.dart';
 import 'generic_sliding_appbar.dart';
 
 class SlidingAppBars extends StatefulWidget {
-  const SlidingAppBars({super.key});
-
+  const SlidingAppBars({
+    super.key,
+    required this.showAppbarController,
+  });
+  final BehaviorSubject<bool> showAppbarController;
   @override
   State<SlidingAppBars> createState() => _SlidingAppBarsState();
 }
 
 class _SlidingAppBarsState extends State<SlidingAppBars>
     with SingleTickerProviderStateMixin {
-  bool _visible = true;
   late final AnimationController _controller;
 
   @override
@@ -31,22 +33,47 @@ class _SlidingAppBarsState extends State<SlidingAppBars>
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 5)).then((value) {
-      setState(() {
-        _visible = !_visible;
-      });
-    });
     return Column(
       children: [
-        SlidingAppBar(
-          slidingOffset: const Offset(0, -1),
-          controller: _controller,
-          visible: _visible,
-          child: AppBar(
-            title: const Text('AppBar'),
-          ),
-        ),
+        StreamBuilder<bool>(
+            stream: widget.showAppbarController.stream,
+            builder: (context, snapshot) {
+              PopupMenuOption? selectedOption;
+              if (snapshot.hasData) {
+                print(snapshot.data);
+                return SlidingAppBar(
+                  slidingOffset: const Offset(0, -1),
+                  controller: _controller,
+                  visible: snapshot.data!,
+                  child: AppBar(
+                    actions: [
+                      PopupMenuButton<PopupMenuOption>(
+                        initialValue: selectedOption,
+                        onSelected: (value) {
+                          print(value);
+                        },
+                        itemBuilder: (context) =>
+                            <PopupMenuEntry<PopupMenuOption>>[
+                          const PopupMenuItem<PopupMenuOption>(
+                            value: PopupMenuOption.share,
+                            child: Text("hhhh"),
+                          )
+                        ],
+                      )
+                    ],
+                    //  backgroundColor: Colors.white,
+                    title: const Text('AppBar'),
+                  ),
+                );
+              } else {
+                return Container();
+              }
+            }),
       ],
     );
   }
+}
+
+enum PopupMenuOption {
+  share,
 }
