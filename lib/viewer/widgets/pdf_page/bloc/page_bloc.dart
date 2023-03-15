2ebe9fe8ac7efc92549584696d18a44c58bad694
stevehighly0 <stevehighly0>
2023-03-15 03:31:13 +0300

    relocated page bloc

---
+++ lib/viewer/widgets/pdf_page/bloc/page_bloc.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pdf_editor/viewer/bloc/page_events.dart';
+import 'package:pdf_editor/viewer/bloc/page_states.dart';
+import 'package:pdf_editor/viewer/widgets/pdf_page/pdf_page.dart';
+
+import '../crud/pdf_to_image_converter.dart';
+import '../crud/text_recognizer.dart';
+
+class PageBloc extends Bloc<PageEvent, PageState> {
+  PageBloc() : super(const PageStateInitial()) {
+    on<PageEventUpdateDisplay>(
+      (event, emit) async {
+        final x = Stopwatch()..start();
+
+        final mainImage = await PdfToImage().getOrUpdateImage(
+          pageNumber: 1,
+          scaleFactor: 3,
+        );
+        HighResolutionPatch? highResolutionPatch;
+        print(event.scaleFactor);
+        if (event.scaleFactor > 3) {
+          highResolutionPatch = await PdfToImage()
+              .createImage(
+                pageNumber: 3,
+                scaleFactor: event.scaleFactor,
+                pageCropRect: event.pageVisibleBounds,
+              )
+              .then((image) => HighResolutionPatch(
+                    image: image,
+                    details: event.pageVisibleBounds,
+                  ));
+
+          print("lasted: ${x.elapsedMilliseconds}");
+        }
+
+        emit(
+          PageStateUpdatingDisplay(
+            scaleFactor: event.scaleFactor,
+            mainImage: await decodeImageFromList(
+                File(mainImage.path).readAsBytesSync()),
+            highResolutionPatch: highResolutionPatch,
+
+            // generate wordcollection only when scaleFactor is 3 or above
+            extractedText: mainImage.scaleFactor >= 3
+                ? event.extractedText ??
+                    await wordCollectionExtractor(
+                        imageFilePath: mainImage.path,
+                        scaleFactor: mainImage.scaleFactor)
+                : null,
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/viewer/widgets/pdf_page/bloc/page_states.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:pdf_editor/viewer/widgets/pdf_page/pdf_page.dart';
+
+import 'dart:ui' as ui;
+import '../crud/text_recognizer.dart';
+
+@immutable
+abstract class PageState extends Equatable {
+  const PageState();
+  @override
+  List<Object?> get props => [];
+}
+
+class PageStateInitial extends PageState {
+  const PageStateInitial();
+}
+
+class PageStateUpdatingDisplay extends PageState {
+  const PageStateUpdatingDisplay({
+    required this.mainImage,
+    required this.scaleFactor,
+    this.highResolutionPatch,
+    this.extractedText,
+  });
+
+  final ui.Image mainImage;
+  final double scaleFactor;
+  final WordCollection? extractedText;
+  final HighResolutionPatch? highResolutionPatch;
+
+  @override
+  List<Object?> get props =>
+      [extractedText, scaleFactor, mainImage, highResolutionPatch];
+}
