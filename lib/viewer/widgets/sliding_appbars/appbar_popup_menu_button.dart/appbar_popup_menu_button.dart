0561a463099d23e2742aafcf19aa9a55d9d69ebc
stevehighly0 <stevehighly0>
2023-03-16 02:40:34 +0300

    created custom popup menu with zero side padding

---
+++ lib/viewer/widgets/sliding_appbars/appbar_popup_menu_button.dart/appbar_popup_menu_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'custom_popupmenubutton.dart';
+
+class AppbarPopupMenuButton extends StatelessWidget {
+  const AppbarPopupMenuButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    PopupMenuOption? selectedOption;
+    return PopupMenuButton<PopupMenuOption>(
+      initialValue: selectedOption,
+      onCanceled: () {},
+      onSelected: (value) {
+        print(value);
+      },
+      itemBuilder: (context) => <PopupMenuEntry<PopupMenuOption>>[
+        const PopupMenuItem<PopupMenuOption>(
+          value: PopupMenuOption.share,
+          child: Text("share"),
+        ),
+        const PopupMenuItem<PopupMenuOption>(
+          value: PopupMenuOption.someThingElse,
+          child: Text("something else"),
+        )
+      ],
+      offset: const Offset(0, 55),
+
+      // clipBehavior: Clip.hardEdge,
+    );
+  }
+}
+
+enum PopupMenuOption { share, someThingElse }
